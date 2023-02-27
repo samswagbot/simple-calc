9530fd170423cbf6a766177aba06c875ea54a33b
Samson <sammossallam@gmail.com>
2023-02-26 19:44:37 -0500

    buttonValues

---
+++ src/data.ts
@@ -0,0 +1,70 @@
+import styles from './App.module.css'
+
+export const buttonValues = [
+    {
+      label: "AC",
+      className: styles.actions,
+    },
+    {
+      label: "+/-",
+      className: styles.actions,
+    },
+    {
+      label: "%",
+      className: styles.actions,
+    },
+    {
+      label: "÷",
+      className: styles.operators,
+    },
+    {
+      label: 7,
+    },
+    {
+      label: 8,
+    },
+    {
+      label: 9,
+    },
+    {
+      label: "×",
+      className: styles.operators,
+    },
+    {
+      label: 4,
+    },
+    {
+      label: 5,
+    },
+    {
+      label: 6,
+    },
+    {
+      label: "-",
+      className: styles.operators,
+    },
+    {
+      label: 1,
+    },
+    {
+      label: 2,
+    },
+    {
+      label: 3,
+    },
+    {
+      label: "+",
+      className: styles.operators,
+    },
+    {
+      label: 0,
+      className: styles.zero,
+    },
+    {
+      label: ".",
+    },
+    {
+      label: "=",
+      className: styles.operators,
+    },
+  ];
